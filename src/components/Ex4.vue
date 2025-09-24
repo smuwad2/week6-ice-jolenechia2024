84c65416db682c4bacf8a424e5a9040fb2327549
Chia Han Xin Jolene <jolene.chia.2024@smu.edu.sg>
2025-09-24 09:31:21 +0000

    done

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,23 +3,28 @@
         data() {
             return {
                 // Add code here
+                change: true,
+                id: "demo1"
             }
         },
         methods: { 
    			// Add code here
-		}
-    }
+            changecolor (){
+                this.change = !this.change
+                }
+         }
+	}
+    
 </script>
 
 <template>
     
     <!-- Modify code here -->
     <div class="container">
-        <div v-bind:id="id" class="m-2">
+        <div  v-bind:class="change ? 'blueBox' : 'redBox'" @click="changecolor" :id="id" class="m-2">
             div ID : {{id}} 
         </div>
-        
-        <button type="button">Change Color</button>
+    <button v-bind:class="change ? 'btn btn-primary' : 'btn btn-danger'" type="button" @click="changecolor">Change color</button>
     </div>
 
 </template>
